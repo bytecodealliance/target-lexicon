49a0242e24cf2b7dc89c25f1fa1f49b01c279bc7
Joshua Nelson <jyn514@gmail.com>
2020-03-12 19:57:53 -0400

    Allow a `Size` where bytes are not 8 bits

--- src/data_model.rs
+++ src/data_model.rs
@@ -1,30 +1,33 @@
-/// The size in bits of a type.
+/// The size of a type.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 #[allow(missing_docs)]
-pub enum Size {
-    U16,
-    U32,
-    U64,
+pub struct Size {
+    /// The size in bits of this type.
+    bits: u8,
+    /// The number of bits in a byte.
+    char_bit: u8,
 }
 
 impl Size {
     /// Return the number of bits in a size.
     pub fn bits(self) -> u8 {
-        match self {
-            Size::U16 => 16,
-            Size::U32 => 32,
-            Size::U64 => 64,
-        }
+        self.bits
     }
 
     /// Return the number of bytes in a size.
     ///
-    /// For these purposes, there are 8 bits in a byte.
+    /// Depending on the target, a byte may not necessarily be 8 bits.
     pub fn bytes(self) -> u8 {
-        match self {
-            Size::U16 => 2,
-            Size::U32 => 4,
-            Size::U64 => 8,
+        let bytes = self.bits / self.char_bit;
+        // make sure that self.bits is a multiple of char_bit
+        assert_eq!(bytes * self.char_bit, self.bits);
+        bytes
+    }
+    /// Create a new `Size`, assuming that `char_bit` is 8.
+    fn with_bits(bits: u8) -> Self {
+        Self {
+            bits,
+            char_bit: 8,
         }
     }
 }
@@ -51,44 +54,44 @@ impl CDataModel {
     /// The width of a pointer (in the default address space).
     pub fn pointer_width(&self) -> Size {
         match self {
-            CDataModel::LP32 | CDataModel::ILP32 => Size::U32,
-            CDataModel::LLP64 | CDataModel::LP64 | CDataModel::ILP64 => Size::U64,
+            CDataModel::LP32 | CDataModel::ILP32 => Size::with_bits(32),
+            CDataModel::LLP64 | CDataModel::LP64 | CDataModel::ILP64 => Size::with_bits(64),
         }
     }
     /// The size of a C `short`. This is required to be at least 16 bits.
     pub fn short_size(&self) -> Size {
         match self {
-            CDataModel::LP32 | CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::LP64 | CDataModel::ILP64 => Size::U16,
+            CDataModel::LP32 | CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::LP64 | CDataModel::ILP64 => Size::with_bits(16),
         }
     }
     /// The size of a C `int`. This is required to be at least 16 bits.
     pub fn int_size(&self) -> Size {
         match self {
-            CDataModel::LP32 => Size::U16,
-            CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::LP64 | CDataModel::ILP64 => Size::U32,
+            CDataModel::LP32 => Size::with_bits(16),
+            CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::LP64 | CDataModel::ILP64 => Size::with_bits(32),
         }
     }
     /// The size of a C `long`. This is required to be at least 32 bits.
     pub fn long_size(&self) -> Size {
         match self {
-            CDataModel::LP32 | CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::ILP64 => Size::U32,
-            CDataModel::LP64 => Size::U64,
+            CDataModel::LP32 | CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::ILP64 => Size::with_bits(32),
+            CDataModel::LP64 => Size::with_bits(64),
         }
     }
     /// The size of a C `long long`. This is required (in C99+) to be at least 64 bits.
     pub fn long_long_size(&self) -> Size {
         match self {
-            CDataModel::LP32 | CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::ILP64 | CDataModel::LP64 => Size::U64,
+            CDataModel::LP32 | CDataModel::ILP32 | CDataModel::LLP64 | CDataModel::ILP64 | CDataModel::LP64 => Size::with_bits(64),
         }
     }
     /// The size of a C `float`.
     pub fn float_size(&self) -> Size {
         // TODO: this is probably wrong on at least one architecture
-        Size::U32
+        Size::with_bits(32)
     }
     /// The size of a C `double`.
     pub fn double_size(&self) -> Size {
         // TODO: this is probably wrong on at least one architecture
-        Size::U64
+        Size::with_bits(64)
     }
 }
